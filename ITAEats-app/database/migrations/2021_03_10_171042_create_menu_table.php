43a8202c5def4aa4315eee626d49248cb3dbbca0
JDV <18151716@aguascalientes.tecnm.mx>
2021-03-10 23:18:30 -0600

    Update Laravel and Information DB
    Agregamos el Framework Laravel, donde esta documentado tambien la base de datos.

---
+++ ITAEats-app/database/migrations/2021_03_10_171042_create_menu_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenuTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu', function (Blueprint $table) {
+            $table->increments('idDePlatillo');
+            $table->text('nombreDePlatillo');
+            $table->text('descripcionDePlatillo');
+            $table->binary('fotoDePlatillo');
+            $table->double('precio');
+            $table->boolean('disponible');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu');
+    }
+}
